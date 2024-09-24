d767c09b8ceb780d494f0b6677e2414aa8345b06
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-09-24 14:16:04 -0300

    chore: seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
 import model.entitites.Department;
+import model.entitites.Seller;
+
+import java.text.ParseException;
+import java.util.Date;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(1, "Bob Purple", "bob.purple@email.com", new Date(),
+                2000.00, obj);
+
+        System.out.println(seller);
     }
 }
